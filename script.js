78db7f9b41d51bfea533f206863fb018c8279dfc
Sahil1036 <141828257+Sahil1036@users.noreply.github.com>
2023-10-15 06:48:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ let list=document.querySelectorAll("ul li");
 let listarr=Array.from(list);
 listarr.forEach((e)=>{
     e.addEventListener("click",()=>{
-        if(screen.width<800){
+        if(screen.width<360){
             navbar.style="display:none";
             menu.forEach((e) => {
              e.style.transform="none";
